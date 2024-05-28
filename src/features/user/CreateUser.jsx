9ce884c0bf6d92264dc4330a8521409f587924b3
aditya <adityakunghadkar94.com>
2024-05-28 19:53:07 +0530

    Updated file

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -9,13 +9,16 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-700 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="w-72 px-1.5"
       />
 
       {username !== '' && (
