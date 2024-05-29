77fd8132296cdd7e432e5ca8f544c08485e24f5d
aditya <adityakunghadkar94.com>
2024-05-29 13:58:21 +0530

    Updated with Flex in Tailwind CSS

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Aditya Kunghadkar</div>;
+  return <div className="hidden text-sm font-semibold md:block">Aditya K.</div>;
 }
 
 export default Username;
