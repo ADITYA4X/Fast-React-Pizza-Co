438561ffe2deed7fda1b8907c63fdb815cf7fe3c
aditya <adityakunghadkar94.com>
2024-06-04 19:59:44 +0530

    Updated with data without navigation

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -10,6 +10,7 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 /*
 const order = {
   id: "ABCDEF",
@@ -58,7 +59,8 @@ function Order() {
     [fetcher],
   );
 
-  console.log(fetcher.data);
+  // console.log(fetcher.data);
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -126,6 +128,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
